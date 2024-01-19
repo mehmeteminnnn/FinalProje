a57ef0c0e941dd56ab8d31d1ad3162b2bea8ef0f
Mehmet Emin Tok <mhmtmntok@gmail.com>
2024-01-19 15:34:21 +0300

    210401072 5
    Q5 özellikleriyle tamamlandı

--- tests/Q1_tests_mehmetemin_tok_210401072.R
+++ tests/Q1_tests_mehmetemin_tok_210401072.R
@@ -1,14 +1,14 @@
-# YC<klC< kC<tC<phaneleri kontrol et
+# Yüklü kütüphaneleri kontrol et
 library(testthat)
 filename <- "Summer-Olympic-medals-1976-to-2008.csv"
 library(readr)
 olympic <- read_csv(filename)
-# Test senaryolarD1nD1 iC'eren bir dosya oluEturun (C6rneDin, test_veri_keEfi.R)
-context("Q3 AdD1mD1 iC'in Test SenaryolarD1: Veri seti keEifsel analizi")
+# Test senaryolarını içeren bir dosya oluşturun (örneğin, test_veri_keşfi.R)
+context("Q3 Adımı için Test Senaryoları: Veri seti keşifsel analizi")
 
-# Test: Veri setinin satD1r ve sC<tun sayD1larD1 doDru
-test_that("Veri setinin satD1r ve sC<tun sayD1larD1 doDru", {
-  # Beklenen deDerler
+# Test: Veri setinin satır ve sütun sayıları doğru
+test_that("Veri setinin satır ve sütun sayıları doğru", {
+  # Beklenen değerler
   beklenen_satir_sayisi <- 15433
   beklenen_sutun_sayisi <- 11
   
@@ -17,27 +17,65 @@ test_that("Veri setinin satD1r ve sC<tun sayD1larD1 doDru", {
   expect_equal(ncol(olympic), beklenen_sutun_sayisi)
 })
 
-# Test: Veri setindeki sC<tun isimleri doDru tanD1mlandD1
-test_that("Veri setindeki sC<tun isimleri doDru tanD1mlandD1", {
-  # Beklenen sC<tun isimleri
+# Test: Veri setindeki sütun isimleri doğru tanımlandı
+test_that("Veri setindeki sütun isimleri doğru tanımlandı", {
+  # Beklenen sütun isimleri
   beklenen_sutun_isimleri <- c("City", "Year", "Sport", "Discipline", "Event", "Athlete", "Gender", "Country_Code", "Country", "Event_gender", "Medal")
   
   # Test
   expect_true(all(colnames(olympic) == beklenen_sutun_isimleri))
 })
-##Q4 KD1smD1
+##Q4 Kısmı
 
 
-test_that("Veri seti C6n iEleme doDru yapD1ldD1", {
-  # Atlet sC<tunu factor tC<rC<ne dC6nC<EtC<rC<ldC< mC<?
-  expect_true(is.factor(olympic$Athlete), info = "Atlet sC<tunu factor tC<rC<ne dC6nC<EtC<rC<ldC< mC<?")
+test_that("Veri seti ön işleme doğru yapıldı", {
+  # Atlet sütunu factor türüne dönüştürüldü mü?
+  expect_true(is.factor(olympic$Athlete), info = "Atlet sütunu factor türüne dönüştürüldü mü?")
   
-  # Czet rapor doDru mu?
+  # Özet rapor doğru mu?
   expected_summary_df <- data.frame(
     unique_values_Athlete = length(unique(olympic$Athlete)),
     unique_values_Event = length(unique(olympic$Event))
   )
   
-  expect_equal(summary_df, expected_summary_df, info = "Czet rapor doDru mu?")
+  expect_equal(summary_df, expected_summary_df, info = "Özet rapor doğru mu?")
 })
+
+##Q5 KISMI
+library(testthat)
+
+# Test: 'Athlete' sütunu factor türüne dönüştürüldü mü?
+test_that("Athlete sütunu factor türüne dönüştürüldü mü?", {
+  expect_true(is.factor(olympic$Athlete), info = "Athlete sütunu factor türüne dönüştürüldü mü?")
+})
+
+# Test: En başarılı ülkelerin data frame'i oluşturuldu mu?
+test_that("En başarılı ülkelerin data frame'i oluşturuldu mu?", {
+  en_basarili_ulke <- olympic %>%
+    filter(Sport == "Handball" & Gender == "Women") %>%
+    group_by(Country) %>%
+    summarise(Medal = n()) %>%
+    top_n(5, Medal)
+  
+  expect_equal(nrow(en_basarili_ulke), 5, info = "Doğru sayıda satır oluşturuldu mu?")
+  expect_equal(colnames(en_basarili_ulke), c("Country", "Medal"), info = "Doğru sütun isimleri oluşturuldu mu?")
+})
+
+# Test: Yüzmede en iyi 10 ülkenin data frame'i oluşturuldu mu?
+test_that("Yüzmede en iyi 10 ülkenin data frame'i oluşturuldu mu?", {
+  swimming_men <- olympic %>%
+    filter(Gender == "Men" & Discipline == "Swimming")
+  
+  medal_counts <- swimming_men %>%
+    group_by(Country) %>%
+    summarise(Total_Medals = n())
+  
+  top_10_countries <- medal_counts %>%
+    arrange(desc(Total_Medals)) %>%
+    top_n(10, Total_Medals)
+  
+  expect_equal(nrow(top_10_countries), 10, info = "Doğru sayıda satır oluşturuldu mu?")
+  expect_equal(colnames(top_10_countries), c("Country", "Total_Medals"), info = "Doğru sütun isimleri oluşturuldu mu?")
+})
+
 
